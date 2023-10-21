4c7da64c7e9b1c2f4c81986df29784433e7a5af6
Shivam-ku539 <Shivam.hms@gmail.com>
2023-10-21 20:39:06 +0530

    checking commi21

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Shivam kumar
+          Learn React with Shivam kumar and check git
         </a>
       </header>
     </div>
