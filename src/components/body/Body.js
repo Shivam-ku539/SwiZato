9eee539cb84980c0aff0094bbe4d9f64f93ea4c0
Shivam-ku539 <Shivam.hms@gmail.com>
2023-10-26 11:17:57 +0530

    Hooked my project with react hook

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import RestCards from './Rest-cards'
 import restaurants from '../../utils/mockData'
 
 
-const resInfo=(restaurants.map((x)=>x.info))
-
 const Body = () => {
+  const [restList,setRestList]=useState(restaurants.map((x)=>x.info))
+  // const resInfo=(restaurants.map((x)=>x.info))
   return (
     <div className='body'>
-        <div className='Search'>
-            SearchBar
+        <div className='filter'>
+            <button className='filter-btn' onClick={()=>{let res1=restList.filter((filtered)=>filtered.avgRating>4)
+            setRestList(res1)}}>Top Rated Restaurants</button>
         </div>
         <div className='res-container'>
-            {resInfo.map((restrnt)=><RestCards key={restrnt.id} restData={restrnt}/>)}
+            {restList.map((restrnt)=><RestCards key={restrnt.id} restData={restrnt}/>)}
         </div>
     </div>
   )
