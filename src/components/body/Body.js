ce41a4edc6c6de44be9475a2ad60194806e2571f
Shivam-ku539 <Shivam.hms@gmail.com>
2023-10-30 12:27:02 +0530

    powered our web with swiggy live data API

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import RestCards from './Rest-cards'
-import restaurants from '../../utils/mockData'
 
 
 const Body = () => {
-  const [restList,setRestList]=useState(restaurants.map((x)=>x.info))
-  // const resInfo=(restaurants.map((x)=>x.info))
+  const [restList,setRestList]=useState([])
+  
+  useEffect(()=>{fetchData()},[])
+
+  const fetchData=async()=>{
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6865235&lng=77.3470746&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json=await data.json()
+    const json1=json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    let json2=json1.map((x)=>x.info)
+    setRestList(json2)
+    // console.log(json2[0].name);
+    // console.log(json1)
+  }
+
+  if(restList.length===0){
+    return<h1>Loading...</h1>
+  }
+
   return (
     <div className='body'>
         <div className='filter'>
