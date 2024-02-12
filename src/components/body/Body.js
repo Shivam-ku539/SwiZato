bf6dffc56659ad966f3731f1537e25b5721855f6
Shivam-ku539 <Shivam.hms@gmail.com>
2024-02-12 12:50:19 +0530

    swiggy api result json structure fix

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -16,11 +16,12 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6865235&lng=77.3470746&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6865235&lng=77.3470746&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
+    console.log(json);
     const json1 =
-     await json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+     await json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
         ?.restaurants;
     let json2 =await json1.map((x) => x.info);
     setRestList(json2);
